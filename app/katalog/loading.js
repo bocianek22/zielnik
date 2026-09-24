daae7cc3ae581cb1084bb4797715b29c27e1fc46
bocianek22 <290825370+bocianek22@users.noreply.github.com>
2026-09-24 22:45:44 +0200

    usu

--- app/katalog/loading.js
+++
@@ -1,5 +0,0 @@
-import Skeleton from '../components/Skeleton';
-
-export default function Loading() {
-  return <main className="page"><h1>Katalog odmian w Polsce</h1><Skeleton /></main>;
-}
--- app/rankings/loading.js
+++
@@ -1,5 +0,0 @@
-import Skeleton from '../components/Skeleton';
-
-export default function Loading() {
-  return <main className="page"><h1>Rankingi</h1><Skeleton /></main>;
-}
